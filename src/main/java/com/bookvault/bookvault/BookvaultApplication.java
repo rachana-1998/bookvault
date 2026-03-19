9113d402bca59e5b69ee8919e898fc68a02df895
rachana-1998 <pandeyrachna5.1998@gmail.com>
2026-03-19 10:30:28 +0530

    firstCommit

---
+++ src/main/java/com/bookvault/bookvault/BookvaultApplication.java
@@ -0,0 +1,13 @@
+package com.bookvault.bookvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookvaultApplication.class, args);
+	}
+
+}
